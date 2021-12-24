f7efab0063e127badc51d25a79b57315097b7c91
Vladimir Oleinikov <dragon2020@inbox.ru>
2021-12-24 22:03:48 +0300

    disable dark theme, remove all Scene related stuff, make new directories for files and reorder files

--- FinanceAdvisor/Support/AppDelegate.swift
+++ FinanceAdvisor/Support/AppDelegate.swift
@@ -11,15 +11,15 @@ import CoreData
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        window.makeKeyAndVisible()
+        
         return true
     }
 
     // MARK: UISceneSession Lifecycle
-
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
